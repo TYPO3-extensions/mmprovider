7d095bc939d77d96203e559f15e4d596b00c295d
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-01 10:58:15 +0000

    Added proper icons
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmprovider/trunk@61292 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -7,20 +7,20 @@ t3lib_extMgm::allowTableOnStandardPages('tx_mmprovider_selection');
 
 $TCA['tx_mmprovider_selection'] = array(
 	'ctrl' => array(
-		'title'     => 'LLL:EXT:mmprovider/locallang_db.xml:tx_mmprovider_selection',		
-		'label'     => 'title',	
+		'title'     => 'LLL:EXT:mmprovider/locallang_db.xml:tx_mmprovider_selection',
+		'label'     => 'title',
 		'tstamp'    => 'tstamp',
 		'crdate'    => 'crdate',
 		'cruser_id' => 'cruser_id',
-		'versioningWS' => TRUE, 
+		'versioningWS' => TRUE,
 		'origUid' => 't3_origuid',
-		'default_sortby' => 'ORDER BY title',	
-		'delete' => 'deleted',	
-		'enablecolumns' => array(		
+		'default_sortby' => 'ORDER BY title',
+		'delete' => 'deleted',
+		'enablecolumns' => array(
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_mmprovider_selection.gif',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY) . 'icon_tx_mmprovider_selection.png',
 	),
 );
 ?>
